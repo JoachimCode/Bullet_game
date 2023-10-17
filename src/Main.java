b99ecc7d610fd2053781cceeb6d9209b0845f948
Joachim <Joachimduong@gmail.com>
2023-10-17 20:24:10 +0200

    Added Layeredpane

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,20 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         JFrame root = new JFrame();
         //root.setLayout(new BorderLayout());
+        JLayeredPane layeredPane = new JLayeredPane();
         Screen screen = new Screen();
-        root.add(screen);
+        root.add(layeredPane);
+        screen.setBounds(0, 0, 1920, 1080);
+        layeredPane.add(screen, JLayeredPane.DEFAULT_LAYER);
+
+        JPanel hud = new JPanel();
+        hud.setBounds(0,0, 1000, 500);
+        hud.setVisible(true);
+        hud.setBackground(Color.red);
+        layeredPane.add(hud, JLayeredPane.PALETTE_LAYER);
 
         Player_character character = new Player_character( screen);
         Level_generator level_one = new Level_generator(1, screen, character);
